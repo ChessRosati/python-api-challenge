7209398bdaf5d53a7b92527189ccfcbde779793d
ChessRosati <chessrosati@gmail.com>
2020-07-16 23:55:14 -0400

    new commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "5aa964ce682052d73a4ab981ac4a71cb"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAWOynAgsdDOksiFjbs9vlu59bLft4DT2g"
